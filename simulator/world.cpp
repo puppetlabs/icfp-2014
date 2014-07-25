625b80b8a4dc189cd9c065f478d53837536362d3
Michael Smith <michael.smith@puppetlabs.com>
2014-07-25 10:59:28 -0700

    Create C++ project.

---
+++ simulator/world.cpp
@@ -0,0 +1,13 @@
+/*
+ * Implement the lambda-man world mechanics: http://icfpcontest.org/specification.html#the-lambda-man-game-rules
+ */
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello World" << endl;
+    return 0;
+}
+
