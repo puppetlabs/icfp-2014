2b5becabf8200b03300125810c58666f8cadc0a1
Justin Holguin <justin.holguin@puppetlabs.com>
2014-07-28 00:44:24 -0700

    Improve CLI behavior
    `lein run <files>` now does a bit more of what you expect it to. You can
    pass it as many files as you like and all .llcoolj files will be
    compiled into a single lambdaman.gcc output file. The same output is
    also printed to stdout so that we can still just pipe to pbcopy.
    
    If you pass it one or more .gfk files, they will each be compiled to a
    different file with the .ghc suffix. A short message gets printed to
    stdout just to show the input -> output pairs.

--- src/icfp2014/core.clj
+++ src/icfp2014/core.clj
@@ -1,7 +1,22 @@
 (ns icfp2014.core
-  (:require [icfp2014.compiler :as compiler]))
+  (:require [icfp2014.compiler :as compiler]
+            [icfp2014.ghc :as ghc]))
 
 (defn -main
-  "I do a whole lot."
+  "I do a whole lot.
+
+  For every .llcoolj file: print compiled code to stdout AND write it to lambdaman.gcc.
+ 
+  For every .gfk file: compile and output a corresponding .ghc file."
   [& files]
-  (println (apply compiler/compile-files files)))
+  (let [llcoolj-files (filter #(.endsWith ^String % "llcoolj") files)
+        ghostface-files (filter #(.endsWith ^String % "gfk") files)]
+    (when (seq llcoolj-files)
+      (let [output (apply compiler/compile-files files)]
+        (println output)
+        (spit "lambdaman.gcc" output)))
+    (when (seq ghostface-files)
+      (doseq [file ghostface-files]
+        (let [out-name (clojure.string/replace file "gfk" "ghc")]
+          (ghc/tag-file file out-name)
+          (println (format "%s -> %s" file out-name)))))))
